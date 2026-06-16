b5c77c7dd97630fc02ae58bb974eb3be9fddd6f2
thunderfleet <157895779+thunderfleet@users.noreply.github.com>
2026-06-16 12:45:13 +0530

    Add files via upload

---
+++ client/src/Components/AnimatedBackground/AnimatedBackground.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './AnimatedBackground.css';
+
+const AnimatedBackground = () => {
+  return (
+    <div className="animated-bg-container">
+      <div className="gradient-mesh"></div>
+      
+      {/* Floating Bubbles */}
+      <div className="bubbles">
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+      </div>
+      
+      {/* Abstract Shapes */}
+      <div className="abstract-shapes">
+        <div className="shape shape-1"></div>
+        <div className="shape shape-2"></div>
+        <div className="shape shape-3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedBackground;
